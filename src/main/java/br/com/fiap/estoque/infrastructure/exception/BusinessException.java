ef5d859a229cbf301ae0c38ae7ecab03bec0d754
Pedro <pedro.argentatii@gmail.com>
2023-10-22 20:01:51 -0300

    style: adjusted formatting

--- src/main/java/br/com/fiap/estoque/infrastructure/exception/BusinessException.java
+++ src/main/java/br/com/fiap/estoque/infrastructure/exception/BusinessException.java
@@ -3,42 +3,42 @@ package br.com.fiap.estoque.infrastructure.exception;
 @SuppressWarnings("serial")
 public class BusinessException extends NestedException {
 
-    private final String resourceId;
-    private final String[] args;
+	private final String resourceId;
+	private final String[] args;
 
-    public BusinessException(String resourceId, String... args) {
-        super(resourceId);
-        this.resourceId = resourceId;
-        this.args = args.clone();
-    }
+	public BusinessException(String resourceId, String... args) {
+		super(resourceId);
+		this.resourceId = resourceId;
+		this.args = args.clone();
+	}
 
-    public BusinessException(String resourceId, String message, Exception exception) {
-        super(message, exception);
-        this.resourceId = resourceId;
+	public BusinessException(String resourceId, String message, Exception exception) {
+		super(message, exception);
+		this.resourceId = resourceId;
 		this.args = null;
-    }
-    
-    public BusinessException(String resourceId, Exception exception, String... args) {
-        super(resourceId, exception);
-        this.resourceId = resourceId;
-        this.args = args.clone();
-    }
+	}
 
-    public String[] getArgs() {
-        return this.args.clone();
-    }
+	public BusinessException(String resourceId, Exception exception, String... args) {
+		super(resourceId, exception);
+		this.resourceId = resourceId;
+		this.args = args.clone();
+	}
 
-    public String getResourceId() {
-        return this.resourceId;
-    }
+	public String[] getArgs() {
+		return this.args.clone();
+	}
 
-    @Override
-    public String getMessage() {
-        return this.getResourceId();
-    }
+	public String getResourceId() {
+		return this.resourceId;
+	}
 
-    @Override
-    public String getLocalizedMessage() {
-        return this.getResourceId();
-    }
+	@Override
+	public String getMessage() {
+		return this.getResourceId();
+	}
+
+	@Override
+	public String getLocalizedMessage() {
+		return this.getResourceId();
+	}
 }
