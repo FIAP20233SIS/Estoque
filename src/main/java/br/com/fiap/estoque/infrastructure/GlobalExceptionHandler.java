cef806c72d92ff6e9e7f5d304525f4c02a4b4614
Pedro <pedro.argentatii@gmail.com>
2023-10-22 20:54:34 -0300

    fix: GlobalExceptionHandler adjusted

--- src/main/java/br/com/fiap/estoque/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/estoque/infrastructure/GlobalExceptionHandler.java
@@ -8,23 +8,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import br.com.fiap.estoque.infrastructure.exception.RecordNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 	/**
-	 * Se em qualquer controller do projeto for lançada uma Exception 'EntityNotFoundException',
-	 * o método 'handleError404' será chamado.
+	 * Se em qualquer controller do projeto for lançada uma Exception
+	 * 'EntityNotFoundException', o método 'handleError404' será chamado.
 	 * 
 	 * @return Not Found exception (404).
 	 **/
 	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<?> handleError404() {
-		return ResponseEntity.notFound().build();
+	public ResponseEntity<ErrorResponse> handleEntityNotFoundError(EntityNotFoundException ex) {
+		ErrorResponse error = new ErrorResponse("Entidade não encontrada", HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 
-    @ExceptionHandler(RecordNotFoundException.class)
-    public ResponseEntity<?> handleRecordNotFoundException(RecordNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
+	@ExceptionHandler(RecordNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException ex) {
+		ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
 }
