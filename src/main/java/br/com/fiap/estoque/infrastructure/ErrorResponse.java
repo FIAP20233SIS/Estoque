006f0eddda759b2d60d903fb66f11932cdb08aeb
Pedro <pedro.argentatii@gmail.com>
2023-10-22 20:17:40 -0300

    project version updated to 3.0.6 and spring-doc updated to 2.10

---
+++ src/main/java/br/com/fiap/estoque/infrastructure/ErrorResponse.java
@@ -0,0 +1,17 @@
+package br.com.fiap.estoque.infrastructure;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+	private String message;
+	private int status;
+
+}
